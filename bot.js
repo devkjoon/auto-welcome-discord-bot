48cdd527ad87c1ec1baf25b8cfb7b574cb380960
devkjoon <devkjoon@gmail.com>
2024-07-01 13:18:52 -0400

    custom auto message reply

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client ({
+const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
@@ -14,10 +14,13 @@ client.once('ready', () => {
 
 client.on('guildMemberAdd', member => {
     const welcomeChannel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
-    const newMemberRole = member.guild.roles.cache.find(role => role.name === 'New Member')
+    const newMemberRole = member.guild.roles.cache.find(role => role.name === 'New Member');
+    const formLink = 'https://discordformsapp.com/form/link'; // Replace with your actual form link
 
     if (welcomeChannel && newMemberRole) {
         member.roles.add(newMemberRole);
-        welcomeChannel.send(`Welcome ${member}! Please fill out this form: `)
+        welcomeChannel.send(`Welcome ${member}! Please fill out this form: ${formLink} and react with 😊 in #roles to get access.`);
     }
-})
+});
+
+client.login('your-bot-token');
